e0630ad1956828c1e0c01da698d74d5fbdd4c636
pavel <paveq2003@yandex.ru>
2025-03-28 01:17:06 +0300

    prepare workspace

--- apps/sberhack/app/app.tsx
+++ apps/sberhack/app/app.tsx
@@ -1,8 +1,10 @@
 // Uncomment this line to use CSS modules
 // import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
+import {SharedUi} from '@sberhack/shared-ui';
 
 export function App() {
+  SharedUi()
   return (
     <div>
       <NxWelcome title="sberhack" />
---
+++ libs/shared-ui/README.md
@@ -0,0 +1,7 @@
+# @sberhack/shared-ui
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @sberhack/shared-ui` to execute the unit tests via [Jest](https://jestjs.io).
