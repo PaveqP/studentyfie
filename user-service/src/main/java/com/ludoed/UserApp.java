6aabef9f51b1628be4786bbf792e81ed408666fd
Дима <datakmakov@gmail.com>
2025-04-13 21:47:11 +0300

    fix(main): исправлено название главного класса

--- user-service/src/main/java/com/ludoed/UserApp.java
+++ user-service/src/main/java/com/ludoed/UserApp.java
@@ -8,8 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(scanBasePackages = {"com.ludoed.agent", "com.ludoed.student"})
 @EnableJpaRepositories(basePackages = {"com.ludoed.student.dao", "com.ludoed.agent.dao"})
 @EntityScan(basePackages = {"com.ludoed.student.model", "com.ludoed.agent.model"})
-public class MainApp {
+public class UserApp {
     public static void main(String[] args) {
-        SpringApplication.run(MainApp.class,args);
+        SpringApplication.run(UserApp.class,args);
     }
 }
