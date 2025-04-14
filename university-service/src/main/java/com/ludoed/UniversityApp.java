93653a712c04b166dc544df66d333918d1dc4a1f
datak <datakmakov@gmail.com>
2025-04-14 12:06:51 +0300

    fix(university-service):подправил конфигурационные файлы

--- university-service/src/main/java/com/ludoed/UniversityApp.java
+++ university-service/src/main/java/com/ludoed/UniversityApp.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.ludoed.university", "com.ludoed.programs"})
-@EnableJpaRepositories(basePackages = {"com.ludoed.university.dao", "com.ludoed.programs.dao"})
-@EntityScan(basePackages = {"com.ludoed.university.model", "com.ludoed.programs.model"})
+@SpringBootApplication(scanBasePackages = "com.ludoed.university")
+@EnableJpaRepositories(basePackages = "com.ludoed.university.dao")
+@EntityScan(basePackages = "com.ludoed.university.model")
 public class UniversityApp {
     public static void main(String[] args) {
         SpringApplication.run(UniversityApp.class,args);
